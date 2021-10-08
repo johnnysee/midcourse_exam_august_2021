35306a00e036544a136a3a4434f20dbe53f86f37
johnnysee <js.johnny.see@gmail.com>
2021-10-08 12:49:43 +0200

    WIP Test is failing and problem fetching data from API

--- cypress/integration/userCanSeeApplicationTitle.feature.js
+++ cypress/integration/userCanSeeApplicationTitle.feature.js
@@ -1,18 +1,30 @@
-describe('Application main view', () => {
+describe("Application main view", () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit("/");
+    cy.intercept("https://api.github.com/search/users?q=*", {
+      fixture: "searchUserFixture.json",
+    });
   });
 
-  it('is expected to contain a titel', () => {
-    cy.get("section[name='title']")
-      .should('contain', 'GitHub Search engine')
+  it("is expected to contain a titel", () => {
+    cy.get("section[name='title']").should("contain", "GitHub Search engine");
   });
 
-  it('is expected to contain a button search', () => {
-    cy.get('[data-cy=button]').should("contain", "Search")
-  })
+  it("is expected to contain a button search", () => {
+    cy.get("[data-cy=search-button]").should("contain", "Search");
+  });
 
-  it('is expected to contain an input field', () => {
-    cy.get('[data-cy=input]').should("be.visible")
-  })
+  it("is expected to contain an input field", () => {
+    cy.get("[data-cy=input]").should("be.visible");
+  });
+
+  describe("User search for an user and display output", () => {
+    it("is expected to search for a user", () => {
+      cy.get("[data-cy=input]").type("barack");
+      cy.get("[data-cy=search-button").click();
+    });
+    it("is expected to display output after searching", () => {
+      cy.get("[data-cy=search-results").should("have.length", 1);
+    });
+  });
 });
--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React, { useEffect, useState } from "react";
+import { Button, Input } from "semantic-ui-react";
+import axios from "axios";
 
 const GHSearch = () => {
+  const [inputSearchField, setInputSearchField] = useState();
+  const [searchResults, setSearchResults] = useState([]);
+
+  const fetchData = async () => {
+    let response = await axios.get(
+      `https://api.github.com/search/users?q=barack`
+    );
+    setSearchResults(response);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      <Input data-cy="input" type="text" name="search" placeholder="Input GH username"/>
-      <Button data-cy= "button" name="search">Search</Button>
+      <Input
+        data-cy="input"
+        type="text"
+        name="search"
+        placeholder="Input GH username"
+      />
+      <Button data-cy="search-button" name="search" onClick={fetchData}>
+        Search
+      </Button>
+      <p data-cy="search-results">{searchResults}</p>
     </>
-  )
-}
+  );
+};
 
-export default GHSearch
+export default GHSearch;
