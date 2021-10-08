3f415a467b465a2fcdaa4b39956c9fc54557f975
johnnysee <js.johnny.see@gmail.com>
2021-10-08 11:09:03 +0200

    Added first spec to test button and input field

--- cypress/integration/userCanSeeApplicationTitle.feature.js
+++ cypress/integration/userCanSeeApplicationTitle.feature.js
@@ -3,8 +3,16 @@ describe('Application main view', () => {
     cy.visit('/')
   });
 
-  it('contains titel', () => {
+  it('is expected to contain a titel', () => {
     cy.get("section[name='title']")
       .should('contain', 'GitHub Search engine')
   });
+
+  it('is expected to contain a button search', () => {
+    cy.get('[data-cy=button]').should("contain", "Search")
+  })
+
+  it('is expected to contain an input field', () => {
+    cy.get('[data-cy=input]').should("be.visible")
+  })
 });
--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -4,8 +4,8 @@ import { Button, Input } from 'semantic-ui-react'
 const GHSearch = () => {
   return (
     <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
+      <Input data-cy="input" type="text" name="search" placeholder="Input GH username"/>
+      <Button data-cy= "button" name="search">Search</Button>
     </>
   )
 }
